95f9e933cdc1f8779de7d68a0131d7caa70e499a
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-06-06 03:39:20 +0100

    Webpage headings created and 'npm start' sucessfully tested with these headings.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      style={{padding: "20px"}}>
+      <h2>Welcome to the Race Startlist page.</h2>
+      <h3>As an organiser, please enter your organiser's title to see a list.</h3>
     </div>
   );
 }
