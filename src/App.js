c332adcccc4964ace897dc3109b77b3e82a5e05c
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-06-06 04:10:15 +0100

    Deleted the unnecesary 'App.css' file.  Ran 'npm install axios'.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import './App.css';
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
-    <div className="App">
+    <div
       style={{padding: "20px"}}>
       <h2>Welcome to the Race Startlist page.</h2>
       <h3>As an organiser, please enter your organiser's title to see a list.</h3>
+      <SearchBar />
     </div>
   );
 }
